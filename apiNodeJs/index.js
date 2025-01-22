94e22fc41aa4e498e46551563a49e74b610c8fc4
Vincent PETT <pett.vincent@gmail.com>
2025-01-22 16:09:04 +0100

    fit: add t7

--- apiNodeJs/index.js
+++ apiNodeJs/index.js
@@ -46,7 +46,7 @@ const db = require("./models");
 require("./routes")(app);
 
 // Set port, listen for requests
-const PORT = 443;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
